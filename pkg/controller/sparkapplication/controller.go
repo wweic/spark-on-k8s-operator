kubeflow/spark-operator

Multi Version Support via Job:  Add Tests + other changes  (#553)
* Add resource request and limits for spark-submit pod

* Minor changes for submission-job-manager + add tests

* Remove un-used fields from spec

* deepcopy funcs

* Update auto-generated code

* PR feedback

* PR comments

* Handling submission failure

pkg/controller/sparkapplication/controller.go
@@ -23,7 +23,6 @@ import (
 	"time"
 
 	"github.com/golang/glog"
-	"github.com/google/uuid"
 	"golang.org/x/time/rate"
 	apiv1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/api/errors"
@@ -35,7 +34,6 @@ import (
 	clientset "k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/kubernetes/scheme"
 	typedcorev1 "k8s.io/client-go/kubernetes/typed/core/v1"
-	batchv1 "k8s.io/client-go/listers/batch/v1"
 	v1 "k8s.io/client-go/listers/core/v1"
 	"k8s.io/client-go/tools/cache"
 	"k8s.io/client-go/tools/record"
@@ -72,9 +70,8 @@ type Controller struct {
 	metrics           *sparkAppMetrics
 	applicationLister crdlisters.SparkApplicationLister
 	podLister         v1.PodLister
-	jobLister         batchv1.JobLister
 	ingressURLFormat  string
-	subJobManager     *submissionJobManager
+	subJobManager     submissionJobManager
 }
 
 // NewController creates a new Controller.
@@ -115,7 +112,6 @@ func newSparkApplicationController(
 		recorder:         eventRecorder,
 		queue:            queue,
 		ingressURLFormat: ingressURLFormat,
-		subJobManager:    &submissionJobManager{kubeClient: kubeClient},
 	}
 
 	if metricsConfig != nil {
@@ -141,8 +137,7 @@ func newSparkApplicationController(
 	controller.podLister = podsInformer.Lister()
 
 	jobLister := informerFactory.Batch().V1().Jobs().Lister()
-	controller.jobLister = jobLister
-	controller.subJobManager = &submissionJobManager{kubeClient: kubeClient, jobLister: jobLister}
+	controller.subJobManager = &realSubmissionJobManager{kubeClient: kubeClient, jobLister: jobLister}
 
 	controller.cacheSynced = func() bool {
 		return crdInformer.Informer().HasSynced() && podsInformer.Informer().HasSynced()
@@ -429,6 +424,11 @@ func shouldRetry(app *v1beta1.SparkApplication) bool {
 				return true
 			}
 		}
+	case v1beta1.FailedSubmissionState:
+		// We retry only if the RestartPolicy is Always. The Submission Job already retries upto the OnSubmissionFailureRetries specified.
+		if app.Spec.RestartPolicy.Type == v1beta1.Always {
+			return true
+		}
 	}
 	return false
 }
@@ -504,10 +504,9 @@ func (c *Controller) syncSparkApplication(key string) error {
 	case v1beta1.PendingSubmissionState:
 		// Check the status of the submission Job and set the application status accordingly.
 		succeeded, completionTime, err := c.subJobManager.hasJobSucceeded(appToUpdate)
-		if err != nil {
-			return err
-		}
+
 		if succeeded != nil {
+			// Submission Job terminated in either success or failure.
 			if *succeeded {
 				c.createSparkUIResources(appToUpdate)
 				appToUpdate.Status.AppState.State = v1beta1.SubmittedState
@@ -515,12 +514,21 @@ func (c *Controller) syncSparkApplication(key string) error {
 				if completionTime != nil {
 					appToUpdate.Status.SubmissionTime = *completionTime
 				}
+				c.recordSparkApplicationEvent(appToUpdate)
 			} else {
 				// Since we delegate submission retries to the Kubernetes Job controller, the fact that the
 				// submission Job failed means all the submission attempts failed. So we set the application
 				// state to FailedSubmission, which is a terminal state.
 				appToUpdate.Status.AppState.State = v1beta1.FailedSubmissionState
+				if err != nil {
+					// Propagate the error if the Submission job ended in failure after retries.
+					appToUpdate.Status.AppState.ErrorMessage = err.Error()
+				}
+				c.recordSparkApplicationEvent(appToUpdate)
 			}
+		} else if err != nil {
+			// Received an error trying to query the status of the Job.
+			return err
 		}
 	case v1beta1.SucceedingState:
 		// The current run of the application has completed, check if it needs to be restarted.
@@ -552,7 +560,16 @@ func (c *Controller) syncSparkApplication(key string) error {
 			appToUpdate.Status.AppState.State = v1beta1.PendingRerunState
 		}
 	case v1beta1.FailedSubmissionState:
-		c.recordSparkApplicationEvent(appToUpdate)
+		// Submission Job terminated in failure, check if the application needs to be retried.
+		if !shouldRetry(appToUpdate) {
+			// Application is not subject to retry. Move to terminal FailedState.
+			appToUpdate.Status.AppState.State = v1beta1.FailedState
+			c.recordSparkApplicationEvent(appToUpdate)
+		} else {
+			// Application is subject to retry. Move to PendingRerunState.
+			appToUpdate.Status.AppState.ErrorMessage = ""
+			appToUpdate.Status.AppState.State = v1beta1.PendingRerunState
+		}
 	case v1beta1.InvalidatingState:
 		// Invalidate the current run and enqueue the SparkApplication for re-submission.
 		if err := c.deleteSparkResources(appToUpdate); err != nil {
@@ -615,20 +632,7 @@ func (c *Controller) submitSparkApplication(app *v1beta1.SparkApplication) *v1be
 		}
 	}
 
-	driverPodName := getDriverPodName(app)
-	submissionID := uuid.New().String()
-	submissionCmdArgs, err := buildSubmissionCommandArgs(app, driverPodName, submissionID)
-	if err != nil {
-		app.Status = v1beta1.SparkApplicationStatus{
-			AppState: v1beta1.ApplicationState{
-				State:        v1beta1.FailedSubmissionState,
-				ErrorMessage: err.Error(),
-			},
-		}
-		return app
-	}
-
-	_, err = c.subJobManager.createSubmissionJob(newSubmission(submissionCmdArgs, app))
+	submissionID, driverPodName, err := c.subJobManager.createSubmissionJob(app)
 	if err != nil {
 		if !errors.IsAlreadyExists(err) {
 			app.Status = v1beta1.SparkApplicationStatus{
@@ -638,6 +642,7 @@ func (c *Controller) submitSparkApplication(app *v1beta1.SparkApplication) *v1be
 				},
 			}
 		}
+		c.recordSparkApplicationEvent(app)
 		return app
 	}
 

pkg/controller/sparkapplication/job_test.go
@@ -1,0 +1,219 @@
+/*
+Copyright 2019 Google LLC
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    https://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package sparkapplication
+
+import (
+	"os"
+	"testing"
+	"time"
+
+	batchv1 "k8s.io/api/batch/v1"
+	"k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/informers"
+	kubeclientfake "k8s.io/client-go/kubernetes/fake"
+
+	"github.com/GoogleCloudPlatform/spark-on-k8s-operator/pkg/apis/sparkoperator.k8s.io/v1beta1"
+	"github.com/stretchr/testify/assert"
+)
+
+func newFakeJobManager(jobs ...*batchv1.Job) submissionJobManager {
+	kubeClient := kubeclientfake.NewSimpleClientset()
+
+	informerFactory := informers.NewSharedInformerFactory(kubeClient, 0*time.Second)
+	lister := informerFactory.Batch().V1().Jobs().Lister()
+	informer := informerFactory.Batch().V1().Jobs().Informer()
+	for _, job := range jobs {
+		if job != nil {
+			informer.GetIndexer().Add(job)
+			kubeClient.BatchV1().Jobs(job.GetNamespace()).Create(job)
+		}
+	}
+	return &realSubmissionJobManager{
+		jobLister:  lister,
+		kubeClient: kubeClient,
+	}
+}
+
+func TestGetSubmissionJob(t *testing.T) {
+	app := &v1beta1.SparkApplication{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "foo",
+			Namespace: "default",
+		},
+		Status: v1beta1.SparkApplicationStatus{},
+	}
+
+	// Case 1: Job doesn't exist.
+	jobManager := newFakeJobManager(nil)
+	jobResult, err := jobManager.getSubmissionJob(app)
+	assert.NotNil(t, err)
+	assert.True(t, errors.IsNotFound(err))
+	assert.Nil(t, jobResult)
+
+	// Case 2: Job exists.
+	job := &batchv1.Job{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "foo-spark-submit",
+			Namespace: "default",
+		},
+	}
+	jobManager = newFakeJobManager(job)
+
+	jobResult, err = jobManager.getSubmissionJob(app)
+	assert.Nil(t, err)
+	assert.NotNil(t, jobResult)
+	assert.Equal(t, job, jobResult)
+}
+
+func TestDeleteSubmissionJob(t *testing.T) {
+	app := &v1beta1.SparkApplication{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "foo",
+			Namespace: "default",
+		},
+		Status: v1beta1.SparkApplicationStatus{},
+	}
+
+	// Case 1: Job doesn't exist.
+	jobManager := newFakeJobManager(nil)
+	err := jobManager.deleteSubmissionJob(app)
+	assert.NotNil(t, err)
+	assert.True(t, errors.IsNotFound(err))
+
+	// Case 2: Job exists
+	job := &batchv1.Job{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "foo-spark-submit",
+			Namespace: "default",
+		},
+	}
+	jobManager = newFakeJobManager(job)
+	err = jobManager.deleteSubmissionJob(app)
+	assert.Nil(t, err)
+}
+
+func TestCreateSubmissionJob(t *testing.T) {
+	os.Setenv(kubernetesServiceHostEnvVar, "localhost")
+	os.Setenv(kubernetesServicePortEnvVar, "443")
+
+	// Case 1: Image doesn't exist.
+	app := &v1beta1.SparkApplication{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "foo",
+			Namespace: "default",
+		},
+		Status: v1beta1.SparkApplicationStatus{},
+	}
+
+	jobManager := newFakeJobManager(nil)
+	submissionID, driverPodName, err := jobManager.createSubmissionJob(app)
+	assert.NotNil(t, err)
+	assert.Empty(t, submissionID)
+	assert.Empty(t, driverPodName)
+
+	// Case 2:  Job creation successful.
+	app = &v1beta1.SparkApplication{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "foo",
+			Namespace: "default",
+		},
+		Spec: v1beta1.SparkApplicationSpec{
+			Image: stringptr("spark-base-image"),
+		},
+		Status: v1beta1.SparkApplicationStatus{},
+	}
+	jobManager = newFakeJobManager(nil)
+	submissionID, driverPodName, err = jobManager.createSubmissionJob(app)
+	assert.Nil(t, err)
+	assert.NotNil(t, submissionID)
+	assert.NotNil(t, driverPodName)
+
+}
+
+func TestHasJobSucceeded(t *testing.T) {
+	app := &v1beta1.SparkApplication{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "foo",
+			Namespace: "default",
+		},
+		Status: v1beta1.SparkApplicationStatus{},
+	}
+
+	// Case 1: Job doesn't exist.
+	jobManager := newFakeJobManager(nil)
+	result, successTime, err := jobManager.hasJobSucceeded(app)
+	assert.NotNil(t, err)
+	assert.True(t, errors.IsNotFound(err))
+	assert.Nil(t, result)
+	assert.Nil(t, successTime)
+
+	// Case 2: Job exists but not completed.
+	job := &batchv1.Job{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "foo-spark-submit",
+			Namespace: "default",
+		},
+	}
+	jobManager = newFakeJobManager(job)
+	result, successTime, err = jobManager.hasJobSucceeded(app)
+	assert.Nil(t, err)
+	assert.Nil(t, result)
+	assert.Nil(t, successTime)
+
+	// Case 3: Job failed.
+	job = &batchv1.Job{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "foo-spark-submit",
+			Namespace: "default",
+		},
+		Status: batchv1.JobStatus{
+			Conditions: []batchv1.JobCondition{{
+				Type:    batchv1.JobFailed,
+				Status:  v1.ConditionTrue,
+				Reason:  "pod failed",
+				Message: "Pod foo-spark-submit-1 failed",
+			}},
+		},
+	}
+	jobManager = newFakeJobManager(job)
+	result, successTime, err = jobManager.hasJobSucceeded(app)
+	assert.Equal(t, err.Error(), "Submission Job Failed. Error: pod failed. Pod foo-spark-submit-1 failed")
+	assert.False(t, *result)
+	assert.Nil(t, successTime)
+
+	// Case 4: Job succeeded.
+	job = &batchv1.Job{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "foo-spark-submit",
+			Namespace: "default",
+		},
+		Status: batchv1.JobStatus{
+			Conditions: []batchv1.JobCondition{{
+				Type:   batchv1.JobComplete,
+				Status: v1.ConditionTrue,
+			}},
+			CompletionTime: &metav1.Time{Time: time.Now()},
+		},
+	}
+	jobManager = newFakeJobManager(job)
+	result, successTime, err = jobManager.hasJobSucceeded(app)
+	assert.Nil(t, err)
+	assert.True(t, *result)
+	assert.NotNil(t, successTime)
+}

wweic/spark-on-k8s-operator

pkg/controller/sparkapplication/controller.go
@@ -22,7 +22,6 @@ import (
 	"time"
 
 	"github.com/golang/glog"
-	"github.com/google/uuid"
 	"golang.org/x/time/rate"
 	apiv1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/api/equality"
@@ -35,7 +34,6 @@ import (
 	clientset "k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/kubernetes/scheme"
 	typedcorev1 "k8s.io/client-go/kubernetes/typed/core/v1"
-	batchv1 "k8s.io/client-go/listers/batch/v1"
 	v1 "k8s.io/client-go/listers/core/v1"
 	"k8s.io/client-go/tools/cache"
 	"k8s.io/client-go/tools/record"
@@ -74,10 +72,9 @@ type Controller struct {
 	metrics           *sparkAppMetrics
 	applicationLister crdlisters.SparkApplicationLister
 	podLister         v1.PodLister
-	jobLister         batchv1.JobLister
 	ingressURLFormat  string
 	batchSchedulerMgr *batchscheduler.SchedulerManager
-	subJobManager     *submissionJobManager
+	subJobManager     submissionJobManager
 }
 
 // NewController creates a new Controller.
@@ -121,7 +118,7 @@ func newSparkApplicationController(
 		queue:             queue,
 		ingressURLFormat:  ingressURLFormat,
 		batchSchedulerMgr: batchSchedulerMgr,
-		subJobManager:     &submissionJobManager{kubeClient: kubeClient},
+		subJobManager:     &realSubmissionJobManager{kubeClient: kubeClient},
 	}
 
 	if metricsConfig != nil {
@@ -147,8 +144,7 @@ func newSparkApplicationController(
 	controller.podLister = podsInformer.Lister()
 
 	jobLister := informerFactory.Batch().V1().Jobs().Lister()
-	controller.jobLister = jobLister
-	controller.subJobManager = &submissionJobManager{kubeClient: kubeClient, jobLister: jobLister}
+	controller.subJobManager = &realSubmissionJobManager{kubeClient: kubeClient, jobLister: jobLister}
 
 	controller.cacheSynced = func() bool {
 		return crdInformer.Informer().HasSynced() && podsInformer.Informer().HasSynced()
@@ -448,6 +444,11 @@ func shouldRetry(app *v1beta2.SparkApplication) bool {
 				return true
 			}
 		}
+	case v1beta2.FailedSubmissionState:
+		// We retry only if the RestartPolicy is Always. The Submission Job already retries upto the OnSubmissionFailureRetries specified.
+		if app.Spec.RestartPolicy.Type == v1beta2.Always {
+			return true
+		}
 	}
 	return false
 }
@@ -522,10 +523,9 @@ func (c *Controller) syncSparkApplication(key string) error {
 	case v1beta2.PendingSubmissionState:
 		// Check the status of the submission Job and set the application status accordingly.
 		succeeded, completionTime, err := c.subJobManager.hasJobSucceeded(appToUpdate)
-		if err != nil {
-			return err
-		}
+
 		if succeeded != nil {
+			// Submission Job terminated in either success or failure.
 			if *succeeded {
 				c.createSparkUIResources(appToUpdate)
 				appToUpdate.Status.AppState.State = v1beta2.SubmittedState
@@ -533,12 +533,21 @@ func (c *Controller) syncSparkApplication(key string) error {
 				if completionTime != nil {
 					appToUpdate.Status.SubmissionTime = *completionTime
 				}
+				c.recordSparkApplicationEvent(appToUpdate)
 			} else {
 				// Since we delegate submission retries to the Kubernetes Job controller, the fact that the
 				// submission Job failed means all the submission attempts failed. So we set the application
 				// state to FailedSubmission, which is a terminal state.
 				appToUpdate.Status.AppState.State = v1beta2.FailedSubmissionState
+				if err != nil {
+					// Propagate the error if the submission Job ended in failure after retries.
+					appToUpdate.Status.AppState.ErrorMessage = err.Error()
+				}
+				c.recordSparkApplicationEvent(appToUpdate)
 			}
+		} else if err != nil {
+			// Received an error trying to query the status of the Job.
+			return err
 		}
 	case v1beta2.SucceedingState:
 		// The current run of the application has completed, check if it needs to be restarted.
@@ -568,7 +577,16 @@ func (c *Controller) syncSparkApplication(key string) error {
 			appToUpdate.Status.AppState.State = v1beta2.PendingRerunState
 		}
 	case v1beta2.FailedSubmissionState:
-		c.recordSparkApplicationEvent(appToUpdate)
+		// Submission Job terminated in failure, check if the application needs to be retried.
+		if !shouldRetry(appToUpdate) {
+			// Application is not subject to retry. Move to terminal FailedState.
+			appToUpdate.Status.AppState.State = v1beta2.FailedState
+			c.recordSparkApplicationEvent(appToUpdate)
+		} else {
+			// Application is subject to retry. Move to PendingRerunState.
+			appToUpdate.Status.AppState.ErrorMessage = ""
+			appToUpdate.Status.AppState.State = v1beta2.PendingRerunState
+		}
 	case v1beta2.InvalidatingState:
 		// Invalidate the current run and enqueue the SparkApplication for re-submission.
 		if err := c.deleteSparkResources(appToUpdate); err != nil {
@@ -638,30 +656,6 @@ func (c *Controller) submitSparkApplication(app *v1beta2.SparkApplication) *v1be
 		}
 	}
 
-	// Use batch scheduler to perform scheduling task before submitting (before build command arguments).
-	if needScheduling, scheduler := c.shouldDoBatchScheduling(app); needScheduling {
-		newApp, err := scheduler.DoBatchSchedulingOnSubmission(app)
-		if err != nil {
-			glog.Errorf("failed to process batch scheduler BeforeSubmitSparkApplication with error %v", err)
-			return app
-		}
-		//Spark submit will use the updated app to submit tasks(Spec will not be updated into API server)
-		app = newApp
-	}
-
-	driverPodName := getDriverPodName(app)
-	submissionID := uuid.New().String()
-	submissionCmdArgs, err := buildSubmissionCommandArgs(app, driverPodName, submissionID)
-	if err != nil {
-		app.Status = v1beta2.SparkApplicationStatus{
-			AppState: v1beta2.ApplicationState{
-				State:        v1beta2.FailedSubmissionState,
-				ErrorMessage: err.Error(),
-			},
-		}
-		return app
-	}
-
 	// Use batch scheduler to perform scheduling task before submitting.
 	if needScheduling, scheduler := c.shouldDoBatchScheduling(app); needScheduling {
 		newApp, err := scheduler.DoBatchSchedulingOnSubmission(app)
@@ -673,7 +667,7 @@ func (c *Controller) submitSparkApplication(app *v1beta2.SparkApplication) *v1be
 		app = newApp
 	}
 
-	_, err = c.subJobManager.createSubmissionJob(newSubmission(submissionCmdArgs, app))
+	submissionID, driverPodName, err := c.subJobManager.createSubmissionJob(app)
 	if err != nil {
 		if !errors.IsAlreadyExists(err) {
 			app.Status = v1beta2.SparkApplicationStatus{
@@ -683,6 +677,7 @@ func (c *Controller) submitSparkApplication(app *v1beta2.SparkApplication) *v1be
 				},
 			}
 		}
+		c.recordSparkApplicationEvent(app)
 		return app
 	}
 

pkg/controller/sparkapplication/job_test.go
@@ -1,0 +1,219 @@
+/*
+Copyright 2019 Google LLC
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    https://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package sparkapplication
+
+import (
+	"os"
+	"testing"
+	"time"
+
+	batchv1 "k8s.io/api/batch/v1"
+	v1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/informers"
+	kubeclientfake "k8s.io/client-go/kubernetes/fake"
+
+	"github.com/GoogleCloudPlatform/spark-on-k8s-operator/pkg/apis/sparkoperator.k8s.io/v1beta2"
+	"github.com/stretchr/testify/assert"
+)
+
+func newFakeJobManager(jobs ...*batchv1.Job) submissionJobManager {
+	kubeClient := kubeclientfake.NewSimpleClientset()
+
+	informerFactory := informers.NewSharedInformerFactory(kubeClient, 0*time.Second)
+	lister := informerFactory.Batch().V1().Jobs().Lister()
+	informer := informerFactory.Batch().V1().Jobs().Informer()
+	for _, job := range jobs {
+		if job != nil {
+			informer.GetIndexer().Add(job)
+			kubeClient.BatchV1().Jobs(job.GetNamespace()).Create(job)
+		}
+	}
+	return &realSubmissionJobManager{
+		jobLister:  lister,
+		kubeClient: kubeClient,
+	}
+}
+
+func TestGetSubmissionJob(t *testing.T) {
+	app := &v1beta2.SparkApplication{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "foo",
+			Namespace: "default",
+		},
+		Status: v1beta2.SparkApplicationStatus{},
+	}
+
+	// Case 1: Job doesn't exist.
+	jobManager := newFakeJobManager(nil)
+	jobResult, err := jobManager.getSubmissionJob(app)
+	assert.NotNil(t, err)
+	assert.True(t, errors.IsNotFound(err))
+	assert.Nil(t, jobResult)
+
+	// Case 2: Job exists.
+	job := &batchv1.Job{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "foo-spark-submit",
+			Namespace: "default",
+		},
+	}
+	jobManager = newFakeJobManager(job)
+
+	jobResult, err = jobManager.getSubmissionJob(app)
+	assert.Nil(t, err)
+	assert.NotNil(t, jobResult)
+	assert.Equal(t, job, jobResult)
+}
+
+func TestDeleteSubmissionJob(t *testing.T) {
+	app := &v1beta2.SparkApplication{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "foo",
+			Namespace: "default",
+		},
+		Status: v1beta2.SparkApplicationStatus{},
+	}
+
+	// Case 1: Job doesn't exist.
+	jobManager := newFakeJobManager(nil)
+	err := jobManager.deleteSubmissionJob(app)
+	assert.NotNil(t, err)
+	assert.True(t, errors.IsNotFound(err))
+
+	// Case 2: Job exists
+	job := &batchv1.Job{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "foo-spark-submit",
+			Namespace: "default",
+		},
+	}
+	jobManager = newFakeJobManager(job)
+	err = jobManager.deleteSubmissionJob(app)
+	assert.Nil(t, err)
+}
+
+func TestCreateSubmissionJob(t *testing.T) {
+	os.Setenv(kubernetesServiceHostEnvVar, "localhost")
+	os.Setenv(kubernetesServicePortEnvVar, "443")
+
+	// Case 1: Image doesn't exist.
+	app := &v1beta2.SparkApplication{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "foo",
+			Namespace: "default",
+		},
+		Status: v1beta2.SparkApplicationStatus{},
+	}
+
+	jobManager := newFakeJobManager(nil)
+	submissionID, driverPodName, err := jobManager.createSubmissionJob(app)
+	assert.NotNil(t, err)
+	assert.Empty(t, submissionID)
+	assert.Empty(t, driverPodName)
+
+	// Case 2:  Job creation successful.
+	app = &v1beta2.SparkApplication{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "foo",
+			Namespace: "default",
+		},
+		Spec: v1beta2.SparkApplicationSpec{
+			Image: stringptr("spark-base-image"),
+		},
+		Status: v1beta2.SparkApplicationStatus{},
+	}
+	jobManager = newFakeJobManager(nil)
+	submissionID, driverPodName, err = jobManager.createSubmissionJob(app)
+	assert.Nil(t, err)
+	assert.NotNil(t, submissionID)
+	assert.NotNil(t, driverPodName)
+
+}
+
+func TestHasJobSucceeded(t *testing.T) {
+	app := &v1beta2.SparkApplication{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "foo",
+			Namespace: "default",
+		},
+		Status: v1beta2.SparkApplicationStatus{},
+	}
+
+	// Case 1: Job doesn't exist.
+	jobManager := newFakeJobManager(nil)
+	result, successTime, err := jobManager.hasJobSucceeded(app)
+	assert.NotNil(t, err)
+	assert.True(t, errors.IsNotFound(err))
+	assert.Nil(t, result)
+	assert.Nil(t, successTime)
+
+	// Case 2: Job exists but not completed.
+	job := &batchv1.Job{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "foo-spark-submit",
+			Namespace: "default",
+		},
+	}
+	jobManager = newFakeJobManager(job)
+	result, successTime, err = jobManager.hasJobSucceeded(app)
+	assert.Nil(t, err)
+	assert.Nil(t, result)
+	assert.Nil(t, successTime)
+
+	// Case 3: Job failed.
+	job = &batchv1.Job{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "foo-spark-submit",
+			Namespace: "default",
+		},
+		Status: batchv1.JobStatus{
+			Conditions: []batchv1.JobCondition{{
+				Type:    batchv1.JobFailed,
+				Status:  v1.ConditionTrue,
+				Reason:  "pod failed",
+				Message: "Pod foo-spark-submit-1 failed",
+			}},
+		},
+	}
+	jobManager = newFakeJobManager(job)
+	result, successTime, err = jobManager.hasJobSucceeded(app)
+	assert.Equal(t, err.Error(), "Submission Job Failed. Error: pod failed. Pod foo-spark-submit-1 failed")
+	assert.False(t, *result)
+	assert.Nil(t, successTime)
+
+	// Case 4: Job succeeded.
+	job = &batchv1.Job{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "foo-spark-submit",
+			Namespace: "default",
+		},
+		Status: batchv1.JobStatus{
+			Conditions: []batchv1.JobCondition{{
+				Type:   batchv1.JobComplete,
+				Status: v1.ConditionTrue,
+			}},
+			CompletionTime: &metav1.Time{Time: time.Now()},
+		},
+	}
+	jobManager = newFakeJobManager(job)
+	result, successTime, err = jobManager.hasJobSucceeded(app)
+	assert.Nil(t, err)
+	assert.True(t, *result)
+	assert.NotNil(t, successTime)
+}